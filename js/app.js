4d21fc16b7e3831382818fd252851fe1f1406772
Oleg Dub <olegdub155@gmail.com>
2020-11-18 11:00:38 +0200

    demo

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 $(function(){
+
+  // fixed Header
 let header = $('#header');
 let intro = $('#intro');
-let introH = intro.innerHeight();
+let introH;
 let scrollPos = $(window).scrollTop();
+let nav = $('#nav')
+let navToggle =$('#navToggle');
 
 $(window).on('scroll load resize', function(){
   introH = intro.innerHeight();
@@ -12,6 +16,35 @@ $(window).on('scroll load resize', function(){
   } else { 
     header.removeClass('fixed'); 
   }
- 
 }); 
+
+// smooth scroll
+$('[data-scroll]').on('click',function(event){
+event.preventDefault();
+let elementId = $(this).data('scroll');
+let elementOffset = $(elementId).offset().top
+nav.removeClass('show');
+$('html,body').animate({
+  scrollTop:elementOffset - 60
+}, 700)
+})
+
+// navtoggle
+navToggle.on('click', function(event){
+  event.preventDefault();
+nav.toggleClass('show')
+})
+
+
+// reviewsSlider
+let slider =$('#reviewsSlider');
+ slider.slick({
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  fade: true,
+  arrows:false,
+  dots:true
+
+});
 });
